186a3f9c4d331994ae2065397300838578accc53
Vishal Raj <vishalrajroy2000@gmail.com>
2019-08-14 13:10:58 +0530

    Updates

---
+++ precache-manifest.50f2dd58e974d976f2ef5dcd0512443a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcd3277b8026eda03d1ef3e943a82d7",
+    "url": "/AIIMS-Website-Project/index.html"
+  },
+  {
+    "revision": "0e4c6f928eb30d0a5062",
+    "url": "/AIIMS-Website-Project/static/css/main.a11e43c0.chunk.css"
+  },
+  {
+    "revision": "61ef6b992247b8217753",
+    "url": "/AIIMS-Website-Project/static/js/2.8fd5b4c7.chunk.js"
+  },
+  {
+    "revision": "0e4c6f928eb30d0a5062",
+    "url": "/AIIMS-Website-Project/static/js/main.bd0a5195.chunk.js"
+  },
+  {
+    "revision": "d3afaa487c05697c8f5e",
+    "url": "/AIIMS-Website-Project/static/js/runtime~main.62145f63.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AIIMS-Website-Project/precache-manifest.50f2dd58e974d976f2ef5dcd0512443a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AIIMS-Website-Project/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
